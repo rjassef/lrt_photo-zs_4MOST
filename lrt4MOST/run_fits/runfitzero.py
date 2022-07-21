c6cf098ac51279c8baa5259aee615982a5a77642
rjassef <rjassef@gmail.com>
2022-07-21 16:28:07 -0400

    Added ncpu control to run_fitzero.py

--- lrt4MOST/run_fits/runfitzero.py
+++ lrt4MOST/run_fits/runfitzero.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
+import multiprocessing
 
-def Runfitzero(phot, catname="fitzero_sample.txt"):
+def Runfitzero(phot, catname="fitzero_sample.txt", ncpu=None):
 
     #First, write the fitzero sample file.
     #Write the fitzero file. 
@@ -17,10 +18,16 @@ def Runfitzero(phot, catname="fitzero_sample.txt"):
         cato.write("\n")
     cato.close()
 
+    #Set the number of CPUs to use.
+    if ncpu is None:
+        ncpu = multiprocessing.cpu_count()-2
+    my_env = os.environ.copy()
+    my_env['OMP_NUM_THREADS'] = "{}".format(ncpu)
+
     #Erase the previous channel.zpc file if it exists.
     subprocess.call(["rm","-f","channel.zpc"])
 
     #Finallys, call the fitzero program. 
-    subprocess.call("{0:s}/lrt4MOST/fortran_codes/run_fitzero {1}".format(os.environ.get('LRT4MOST_LOC'),catname),shell=True)
+    subprocess.call(["{0:s}/lrt4MOST/fortran_codes/run_fitzero".format(os.environ.get('LRT4MOST_LOC')),catname],env=my_env)
 
     return
