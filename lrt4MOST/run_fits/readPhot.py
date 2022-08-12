b5a8633ca6334bdb35695d5f04626ebf17b47b2b
rjassef <roberto.assef@mail.udp.cl>
2022-08-12 16:17:00 -0400

    Added flux limit to ReadPhot

--- lrt4MOST/run_fits/readPhot.py
+++ lrt4MOST/run_fits/readPhot.py
@@ -6,7 +6,7 @@ from .procPhot import ProcPhot
 
 class ReadPhot(ProcPhot):
 
-    def __init__(self, fname, n=1, idcol='id', zcol='redshift', input_unit='mJy', output_flux_unit='Jy', noise_floor=0.1):
+    def __init__(self, fname, n=1, idcol='id', zcol='redshift', input_unit='mJy', output_flux_unit='Jy', noise_floor=0.1, flux_min=None, flux_min_band=None):
 
         #Open the file. 
         tab = fits.open(fname, format=format)
@@ -20,16 +20,22 @@ class ReadPhot(ProcPhot):
         self.photcols = np.array(self.photcols)
         self.nchan = len(self.photcols)
 
+        if flux_min is None:
+            iuse = np.ones(tab[1].data[idcol].shape, dtype=bool)
+        else:
+            iuse = (~np.isnan(tab[1].data[flux_min_band])) & (tab[1].data[flux_min_band]>=flux_min.to(u.Unit(input_unit)).value)
+
+
         #Now, save the photometry, the IDs and the redshifts.
-        self.id    = tab[1].data[idcol]
+        self.id    = tab[1].data[idcol][iuse]
         self.nobj  = len(self.id)
-        self.zspec = tab[1].data[zcol]
+        self.zspec = tab[1].data[zcol][iuse]
         self.jy    = np.zeros((self.nchan, self.nobj))
         self.ejy   = np.zeros(self.jy.shape)
         flux_factor = (u.Unit(input_unit)/u.Unit(output_flux_unit)).to(1)
         for k, col in enumerate(self.photcols):
-            self.jy[k]  = tab[n].data[col] * flux_factor
-            self.ejy[k] = tab[n].data[col+"_err"] * flux_factor
+            self.jy[k]  = tab[n].data[col][iuse] * flux_factor
+            self.ejy[k] = tab[n].data[col+"_err"][iuse] * flux_factor
 
         #Close the table.
         tab.close()
